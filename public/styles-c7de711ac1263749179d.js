7056187735481129f33f50236b3ec75be487434d
ossoso <lossososoi@gmail.com>
2020-03-12 02:37:47 +0200

    remove gatsby add Create-React-App

--- public/styles-c7de711ac1263749179d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{ekxT:function(n,o,w){},j4Go:function(n,o,w){},wWd4:function(n,o,w){}}]);
-//# sourceMappingURL=styles-c7de711ac1263749179d.js.map
